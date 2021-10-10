e7cc208617b9b5d57d9c098cbcfea189492f9d89
pjroeder <pjroeder@iu.edu>
2021-10-10 17:51:08 -0400

    project part 1

---
+++ Global.gd
@@ -0,0 +1,27 @@
+extends Node
+
+var score = 0
+signal changed
+var scores = {
+	0:0,
+	1:0,
+	2:0,
+	3:10,
+	4:20,
+	5:50,
+	6:100,
+	7:200,
+	8:300,
+	9:1000
+}
+
+func _unhandled_input(event):
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+
+func change_score(s):
+	var camera = get_node_or_null("/root/Game/Camera")
+	if camera:
+		camera.add_trauma(0.5)
+	score += s
+	emit_signal("changed")
