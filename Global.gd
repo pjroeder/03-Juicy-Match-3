7d901c990dc8ebf5a3c8e462884321eefe4f33f7
pjroeder <pjroeder@iu.edu>
2021-10-10 20:02:59 -0400

    project part 2

--- Global.gd
+++ Global.gd
@@ -1,7 +1,19 @@
 extends Node
 
 var score = 0
-signal changed
+var time = 0
+
+var level = 1
+
+var level1_default = 60
+var level2_default = 2000
+
+var level1_score = 0
+var level2_time = 0
+
+signal score_changed
+signal time_changed
+
 var scores = {
 	0:0,
 	1:0,
@@ -15,6 +27,10 @@ var scores = {
 	9:1000
 }
 
+func _ready():
+	if level == 1:
+		time = level1_default
+
 func _unhandled_input(event):
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
@@ -24,4 +40,21 @@ func change_score(s):
 	if camera:
 		camera.add_trauma(0.5)
 	score += s
-	emit_signal("changed")
+	if level == 2:
+		if score >= level2_default:
+			level2_time = time
+			var _new_scene = get_tree().change_scene("res://UI/End_Game.tscn")
+	emit_signal("score_changed")
+
+func change_time():
+	if level == 1:
+		time -= 1
+		if time <= 0:
+			level1_score = score
+			level = 2
+			time = 0
+			score = 0
+			var _new_scene = get_tree().change_scene("res://Game2.tscn")
+	else:
+		time += 1
+	emit_signal("time_changed")
